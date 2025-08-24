dc9de050e580ab8157ac6b0f5bb23643eee5dec9
Arti Awari <awariarti0407@gmail.com>
2025-08-24 21:32:59 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,39 @@
+/*       
+*
+***
+*****
+*******
+*********
+*/
+public class Pattern1 {
+    public void print1(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=2*i-1;j++){
+            System.out.print("*");
+        }
+         System.out.println();
+    }
+    }
+    public void print2(){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=2*i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public void print3(){
+        for(int i=0;i<5;i++){
+            for(int j=1;j<=2*i+1;j++){
+                System.out.print("*");
+            }
+             System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Pattern1 p=new Pattern1();
+        p.print1();
+        p.print2();
+        p.print3();
+    }
+}
